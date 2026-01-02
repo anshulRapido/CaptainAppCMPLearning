795d167339730834ee3afe0aa0c693d913de2aaa
SuryakantRapido <113968568+SuryakantRapido@users.noreply.github.com>
2026-01-02 19:41:42 +0530

    add Divider

--- app/src/main/java/com/rapido/captainapp/presentation/History/OrderHistoryScreen.kt
+++ app/src/main/java/com/rapido/captainapp/presentation/History/OrderHistoryScreen.kt
@@ -1,5 +1,7 @@
 package com.rapido.captainapp.presentation.History
 
+import android.graphics.Color
+import android.icu.lang.UCharacter
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,10 +9,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,6 +24,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineBreak
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.rapido.captainapp.ui.theme.Pink80
@@ -68,6 +73,16 @@ fun OrderHistory(
                 ) {
                     items(state.orders.size) { index ->
                         OrderHistoryItem(state.orders[index])
+                        if (index != state.orders.size - 1) {
+                            Spacer(modifier = Modifier.height(2.dp))
+
+                            Divider(
+                                modifier = Modifier.fillMaxWidth(),
+                                thickness = 1.dp,
+                                color = Purple40
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
                     }
                 }
             }
