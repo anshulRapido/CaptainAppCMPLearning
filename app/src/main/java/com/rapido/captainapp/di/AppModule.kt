a4b470431de34efaa3e9a7357f7076507b7048ca
Anshul Baurai <anshul.baurai@rapido.bike>
2026-01-06 16:09:27 +0530

    Made usecases factory
    resolve conflicct

--- app/src/main/java/com/rapido/captainapp/di/AppModule.kt
+++ app/src/main/java/com/rapido/captainapp/di/AppModule.kt
@@ -37,7 +37,10 @@ val appModule = module {
     factory { RejectOrderUseCase(get()) }
     factory { UpdateOrderStatusUseCase(get()) }
     factory { OrderUseCase(get()) }
+<<<<<<< HEAD
     factory { OrderHistoryUseCase(get()) }
+=======
+>>>>>>> 1e5cf50 (Made usecases factory)
 
     // ViewModels
     viewModel {
